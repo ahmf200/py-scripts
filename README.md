3f1f423429b05ab9121d670cd65d3d9528452411
ahmf200 <fahad15@live.co.uk>
2021-04-02 09:58:18 +0100

    Refactored dir-check and get-robots with better robustness (#4)

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
-# directory-check
+## Usage for dir-check:
+`python dir-check.py url wordlist_file
+`
 
-Usage:
+Example:
+`python dir-check.py https://example.com/ wordlist.txt`
 
-`python dir-check.py <url> <wordlist>`
+
+## Usage for get-robots:
+`python get-robots.py`
+
+It asks the user to provide a url, to check the response of /robots.txt.
+The result will output the robots file and write it to this file --> url_passed--robots-content.txt.
+
--- get-robots.py
+++ get-robots.py
@@ -2,22 +2,28 @@ import requests
 import sys
 
 
+""" 
+Get url from user input and make a request to /robots.txt
+Then check to see if response is successful (200 code) and run the file_to_write_valid_directories function
+"""
 def get_robots_from_single_url():
     ask_user_for_url = input("Enter the url you want to get the robots.txt file from. Please include http or https: ")
-    parse_url_to_name_file = ask_user_for_url.replace('http://', '').replace('.co.uk', '')
     add_robots_to_given_url = ask_user_for_url + "/robots.txt"
-    get_robots_response = requests.get(add_robots_to_given_url, verify=False)
+    get_robots_response = requests.get(add_robots_to_given_url)
     get_robots_response_code = get_robots_response.status_code
-    # print(type(get_robots_response_code))
-    # print(get_robots_response.text)
 
     if get_robots_response_code == 200:
         print(get_robots_response.text)
-        file_to_write_valid_directories(parse_url_to_name_file, get_robots_response.text)
+        file_to_write_valid_directories(ask_user_for_url, get_robots_response.text)
     else:
         print("That request wasn't successful. It looks like that url can't resolve a robots.txt file.")
         sys.exit()
 
+
+"""
+Stripping http/https and www. This isn't necessary, but it makes identifying files easier
+Then writing the response to a file
+"""
 
 def file_to_write_valid_directories(prepend_filename, response):
     protocols = ['http://', 'https://']
